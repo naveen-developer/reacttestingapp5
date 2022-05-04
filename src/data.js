e7ec50bdc749a02f0c46c6302f96bd1a184bafb1
Naveen <naveenofficial100@gmail.com>
2022-05-04 22:31:51 +0530

    second commit

--- src/data.js
+++ src/data.js
@@ -8,26 +8,26 @@ const data = [
         comment: "first commnet",
         likes: 20,
       },
-      // {
-      //   image: "images/vijay2.jpg",
-      //   comment: "second commnet",
-      //   likes: 10
-      // },
-      // {
-      //   image: "images/vijay3.jpg",
-      //   comment: "third commnet",
-      //   likes: 30
-      // },
-      // {
-      //   image: "images/vijay4.jpg",
-      //   comment: "fourth commnet",
-      //   likes: 10
-      // },
-      // {
-      //   image: "images/vijay5.jpg",
-      //   comment: "fifth commnet",
-      //   likes: 50
-      // }
+      {
+        image: "images/vijay2.jpg",
+        comment: "second commnet",
+        likes: 10,
+      },
+      {
+        image: "images/vijay3.jpg",
+        comment: "third commnet",
+        likes: 30,
+      },
+      {
+        image: "images/vijay4.jpg",
+        comment: "fourth commnet",
+        likes: 10,
+      },
+      {
+        image: "images/vijay5.jpg",
+        comment: "fifth commnet",
+        likes: 50,
+      },
     ],
   },
 ];
